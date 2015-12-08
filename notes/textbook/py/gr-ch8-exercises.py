d33c93c5826456abdfcc96f3b0d8797e891dfa3f
Daniel Wysocki <dwysocki@oswego.edu>
2015-12-07 22:22:10 -0500

    Adding more solutions

--- notes/textbook/py/gr-ch8-exercises.py
+++ notes/textbook/py/gr-ch8-exercises.py
@@ -5,6 +5,7 @@ from sys import argv
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.constants.constants import c
 
 
 def problem_3(dir, fmt):
@@ -25,6 +26,8 @@ def problem_3(dir, fmt):
     def a(M, r):
         return phi(M, r) / r
 
+    print("P 8.3.")
+
     print("a)")
     print("    (i) phi =", phi(M_s, R_s))
     print("   (ii) phi =", phi(M_s,  AU))
@@ -38,9 +41,40 @@ def problem_3(dir, fmt):
     print("  (Earth) a =", a(M_e, R_e))
 
 
+def problem_17(dir, fmt):
+    M_s = 1.476e+03
+
+    C = np.array([2.5e+6, 6.3e+6, 6.3e+7, 3.1e+8, 6.3e+9])
+    T = np.array([8.4e-3, 5.5e-2, 2.1e+0, 2.3e+1, 2.1e+3]) * c
+
+    def M(C, T):
+        return C**3 / (2 * np.pi * T**2) / M_s
+
+    M_bh_est = M(C, T)
+
+    fig, ax = plt.subplots()
+
+    ax.scatter(C, M_bh_est)
+
+    ax.set_xlabel("Circumference (m)")
+    ax.set_ylabel(r"$M_\bullet$ ($M_\odot$)")
+
+    ax.set_xlim([0, 7e9])
+
+    fig.savefig(path.join(dir, "ch8_problem_17b."+fmt))
+    plt.close(fig)
+
+    print("P 8.17.")
+
+    print("b)")
+
+    print("C_sat =", C[-1])
+    print("M_bh  =", M_bh_est[-1])
+
 
 problems = [
-    problem_3
+    problem_3,
+    problem_17
 ]
 
 def main(dir, fmt):
